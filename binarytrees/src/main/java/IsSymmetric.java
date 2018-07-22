122ebbdad1855db436f767d85d231e768bf79095
Clement Ng <clementng@berkeley.edu>
2018-07-22 11:05:26 -0700

    Finished isSymmetric

--- binarytrees/src/main/java/IsSymmetric.java
+++ binarytrees/src/main/java/IsSymmetric.java
@@ -5,8 +5,18 @@ public class IsSymmetric {
     */
 
     public static boolean isSymmetric(BinaryTree<Integer> tree) {
-
-        return false;
+//      Need a recursive solution that compares subtrees rather than null to true in default
+        return tree == null || checkSymmetric(tree.left, tree.right);
     }
 
+    private static boolean checkSymmetric(BinaryTree<Integer> left, BinaryTree<Integer> right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left != null && right != null) {
+            return left.data.equals(right.data) &&
+                    checkSymmetric(left.left, right.right) &&
+                    checkSymmetric(right.left, left.right);
+        }
+        return false;
+    }
 }
