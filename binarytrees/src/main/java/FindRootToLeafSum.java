8a017c2cfd167f3fb69b2de41ded85b6d2305466
Clement Ng <clementdng@gmail.com>
2018-07-26 22:10:07 -0700

    Finished hasPathSum

--- binarytrees/src/main/java/FindRootToLeafSum.java
+++ binarytrees/src/main/java/FindRootToLeafSum.java
@@ -5,8 +5,17 @@ public class FindRootToLeafSum {
     */
 
     public static boolean hasPathSum(BinaryTree<Integer> tree, int targetSum) {
-
-        return false;
+        return hasPathSumHelper(tree, targetSum);
+    }
+    private static boolean hasPathSumHelper(BinaryTree<Integer> tree, int targetSum) {
+        if (tree == null) {
+            return false;
+        }
+        targetSum -= tree.data;
+        if (tree.left == null && tree.right == null) {
+            return targetSum == 0;
+        }
+        return hasPathSumHelper(tree.left, targetSum) || hasPathSumHelper(tree.right, targetSum);
     }
 
 }
