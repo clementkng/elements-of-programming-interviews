c65754a0f70fdaf43687987342d7cf891b2cb564
Clement Ng <clementng@berkeley.edu>
2018-07-27 01:15:16 -0700

    Finished InorderIterative

--- binarytrees/src/main/java/InorderIterative.java
+++ binarytrees/src/main/java/InorderIterative.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class InorderIterative {
 
@@ -8,7 +10,31 @@ public class InorderIterative {
     */
 
     public static List<Integer> BSTInOrder(BinaryTree<Integer> tree) {
-
-        return Collections.emptyList();
+        // Recursive
+//        if (tree != null) {
+//            BSTInOrder(tree.left);
+//            int data = tree.data;
+//            BSTInOrder(tree.right);
+//        }
+        // Add left then pop and process current, then process right after pop
+        if (tree == null) {
+            return Collections.emptyList();
+        }
+        Stack<BinaryTree<Integer>> callStack = new Stack<>();
+        BinaryTree<Integer> curr = tree;
+        List<Integer> iot = new ArrayList<>();
+        while (!callStack.isEmpty() || curr != null) {
+            if (curr != null) {
+                // Move left and store until can't
+                callStack.add(curr);
+                curr = curr.left;
+            } else {
+                // Pop off stack, then add current, and move right (if necessary)
+                curr = callStack.pop();
+                iot.add(curr.data);
+                curr = curr.right;
+            }
+        }
+        return iot;
     }
 }
