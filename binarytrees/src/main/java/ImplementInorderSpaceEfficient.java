1cd1654c0de77391ec33513331dbb21d0391553a
Clement Ng <clementdng@gmail.com>
2018-07-30 19:31:08 -0700

    Finished ImplementInorderSpaceEfficient

--- binarytrees/src/main/java/ImplementInorderSpaceEfficient.java
+++ binarytrees/src/main/java/ImplementInorderSpaceEfficient.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,36 @@ public class ImplementInorderSpaceEfficient {
     */
 
     public static List<Integer> inorderTraversal(BinaryTreeParent<Integer> tree) {
+        BinaryTreeParent<Integer> prev = null, curr = tree;
+        List<Integer> ioT = new ArrayList<>();
+        while (curr != null) {
+            BinaryTreeParent<Integer> next;
+            if (curr.parent == prev) {
+                if (curr.left != null) {
+                    next = curr.left;
+                } else {
+                    ioT.add(curr.data);
+                    next = (curr.right != null) ? curr.right : curr.parent;
+                }
+            } else if (curr.left == prev) {
+                ioT.add(curr.data);
+                next = (curr.right != null) ? curr.right : curr.parent;
+            } else {
+                next = curr.parent;
+            }
+            prev = curr;
+            curr = next;
+        }
+        return ioT;
 
-        return Collections.emptyList();
+        // Functional
+//        List<Integer> ioT = new ArrayList<>();
+//        while (tree.left != null) {
+//            tree = tree.left;
+//        }
+//        while (tree != null) {
+//            ioT.add(tree.data);
+//            tree = ComputeSuccessor(tree);
+//        }
     }
 }
