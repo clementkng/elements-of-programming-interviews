e0b24525b8f8eade6ea3310fcf859daca22523ab
Clement Ng <clementdng@gmail.com>
2018-08-05 00:05:06 -0700

    Finished ReverseSingleSublist

--- linkedlists/src/main/java/ReverseSingleSublist.java
+++ linkedlists/src/main/java/ReverseSingleSublist.java
@@ -5,7 +5,22 @@ public class ReverseSingleSublist {
     */
 
     public static ListNode<Integer> reverseSublist(ListNode<Integer> input, int s, int f) {
-
-        return new ListNode<>(null);
+        if (s == f) {
+            return input;
+        }
+        ListNode<Integer> dummyHead = new ListNode<>(0, input);
+        ListNode<Integer> subListHead = dummyHead;
+        int k = 1;
+        while (k++ < s) {
+            subListHead = subListHead.next;
+        }
+        ListNode<Integer> subListIter = subListHead.next;
+        while (s++ < f) {
+            ListNode<Integer> temp = subListIter.next;
+            subListIter.next = temp.next;
+            temp.next = subListHead.next;
+            subListHead.next = temp;
+        }
+        return dummyHead.next;
     }
 }
