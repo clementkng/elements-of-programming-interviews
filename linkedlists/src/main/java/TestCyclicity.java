58b6c7d9f13ada2193beccce2512b1f3f1ad519b
Clement Ng <clementdng@gmail.com>
2018-08-06 18:57:53 -0700

    Finished TestCyclicity

--- linkedlists/src/main/java/TestCyclicity.java
+++ linkedlists/src/main/java/TestCyclicity.java
@@ -5,7 +5,46 @@ public class TestCyclicity {
     */
 
     public static ListNode<Integer> isCyclic(ListNode<Integer> list) {
-
-        return new ListNode<>(null);
+//        ListNode<Integer> slow = list;
+//        ListNode<Integer> fast = list;
+//        while (fast != null && fast.next != null) {
+//            slow = slow.next;
+//            fast = fast.next.next;
+//            if (slow == fast) {
+//                int cycleLen = 0;
+//                do {
+//                    ++cycleLen;
+//                    fast = fast.next;
+//                } while (slow != fast);
+//                ListNode<Integer> cycleLenAdvancedIter = list;
+//                while (cycleLen-- > 0) {
+//                    cycleLenAdvancedIter = cycleLenAdvancedIter.next;
+//                }
+//                ListNode<Integer> iter = list;
+//                while (iter != cycleLenAdvancedIter) {
+//                    iter = iter.next;
+//                    cycleLenAdvancedIter = cycleLenAdvancedIter.next;
+//                }
+//                return iter;
+//            }
+//        }
+//        return null;
+//    }
+        ListNode<Integer> slow = list;
+        ListNode<Integer> fast = list;
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = list;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
     }
+
 }
